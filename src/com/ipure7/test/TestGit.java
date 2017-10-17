1b0505cdee4f609fc3bf311e42367992628996e5
sue <surayisme@126.com>
2017-10-17 10:35:41 +0800

    测试

---
+++ src/com/ipure7/test/TestGit.java
@@ -0,0 +1,14 @@
+package com.ipure7.test;
+/**
+ * 
+ * yds-phone
+ * com.ipure7.test
+ * 2017Äê10ÔÂ17ÈÕ
+ * @author sue
+ *
+ */
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("hello,WORLD!");
+	}
+}
